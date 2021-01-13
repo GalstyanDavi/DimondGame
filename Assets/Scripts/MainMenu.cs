9574c2b90dc45b03567e921baa3a5ec2b5cf956b
GalstyanDavi <63391575+GalstyanDavi@users.noreply.github.com>
2021-01-14 02:20:59 +0400

    First commit
    Added Unity game to Github

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject ThanksText;
+
+    public void Start()
+    {
+        if (GameManager.GameWon == true)
+        {
+            ThanksText.SetActive(true);
+        }
+    }
+
+    public void LoadLevel()
+    {
+        GameManager.Score = 0;
+        GameManager.Lives = 3;
+        SceneManager.LoadScene(1);
+    }
+    
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
